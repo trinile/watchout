a779df44ef13dd968190bab029368e09637940df
trinile <trinile@gmail.com>
2016-03-11 17:00:59 -0800

    Add movement to enemies.

--- client/watchout.js
+++ client/watchout.js
@@ -1,7 +1,8 @@
 
+//create enemies in random positions
 
 
-var createEnemies = function(n) {
+var randomPositions = function(n) {
   var positions = [];
 
   for ( var i = 0; i < n; i++ ) {
@@ -14,13 +15,58 @@ var createEnemies = function(n) {
 };
 
 
-var svg = d3.select(".board").append("svg")
-  .attr({"width": 900, "height": 600} );
 
-svg.selectAll("circle")
-  .data(createEnemies(30))
-  .enter().append("circle")
-  .attr("cx", function(d) { return d.x; })
-  .attr("cy", function(d) { return d.y; })
-  .attr("r", 10);
+// create the scg element
+var svg = d3.select(".board").append("svg")
+  .attr({"width": 900, "height": 600} ); 
+ 
+
+var moveEnemies = function(data) {
+
+  var enemies = svg.selectAll("circle")
+    .data(data);
+
+
+
+  enemies
+    .transition()
+    .duration(750)
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) {return d.y; })
+    .attr("r", 10);
+//updating new elements
+  enemies.enter().append('circle')
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) {return d.y; })
+  .attr('r', 10)
+  .transition()
+  .duration(750);
+
+//   updateEnemies.exit().remove();
+};
+
+setInterval(function() {
+  moveEnemies(randomPositions(30));
+}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
