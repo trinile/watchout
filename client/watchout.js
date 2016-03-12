4e33973d06d71f9fa396e3d37886c0449207c8bf
trinile <trinile@gmail.com>
2016-03-11 17:30:45 -0800

    Add dragable player circle.

--- client/watchout.js
+++ client/watchout.js
@@ -16,33 +16,51 @@ var randomPositions = function(n) {
 
 
 
-// create the scg element
+// create the svg element
 var svg = d3.select(".board").append("svg")
   .attr({"width": 900, "height": 600} ); 
  
+// create drag event 
+var drag = d3.behavior.drag()  
+ 
+ .on('drag', function() { circle.attr('cx', d3.event.x)
+ .attr('cy', d3.event.y); });
+
+
+ // create player
+var circle = svg.selectAll(('.player'))
+  .data([{ x: 450, y: 300}])
+  .enter()
+  .append('svg:circle')
+  .attr('class', 'player')
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) { return d.y; })
+  .attr('r', 10)
+  .call(drag)
+  .style('fill', 'orange');
+
 
 var moveEnemies = function(data) {
 
-  var enemies = svg.selectAll("circle")
+  var enemies = svg.selectAll('.enemies')
     .data(data);
 
 
-
-  enemies
-    .transition()
+//existing enemies
+  enemies.transition()
     .duration(750)
     .attr('cx', function(d) { return d.x; })
     .attr('cy', function(d) {return d.y; })
     .attr("r", 10);
-//updating new elements
-  enemies.enter().append('circle')
+
+  //updating new elements
+  enemies.enter().append('svg:circle')
+  .attr('class', 'enemies')
   .attr('cx', function(d) { return d.x; })
   .attr('cy', function(d) {return d.y; })
   .attr('r', 10)
   .transition()
   .duration(750);
-
-//   updateEnemies.exit().remove();
 };
 
 setInterval(function() {
