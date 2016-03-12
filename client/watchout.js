e86aed57d4e300d8fc5d0be63218828f0948e40f
trinile <trinile@gmail.com>
2016-03-11 16:10:02 -0800

    Complete svg enemies.

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,26 @@
-// start slingin' some d3 here.
+
+
+
+var createEnemies = function(n) {
+  var positions = [];
+
+  for ( var i = 0; i < n; i++ ) {
+    var x = Math.random() * 900;
+    var y = Math.random() * 600;
+    positions.push({"x": x, "y": y});
+  }
+
+  return positions;
+};
+
+
+var svg = d3.select(".board").append("svg")
+  .attr({"width": 900, "height": 600} );
+
+svg.selectAll("circle")
+  .data(createEnemies(30))
+  .enter().append("circle")
+  .attr("cx", function(d) { return d.x; })
+  .attr("cy", function(d) { return d.y; })
+  .attr("r", 10);
+
